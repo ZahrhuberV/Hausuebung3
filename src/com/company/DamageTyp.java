70661227ac2752fb0665b49b60dbdb718e6d077b
ZahrhuberV <valentin.zahrhuber@gmx.at>
2022-09-30 12:06:46 +0200

    Weapons erstellt

---
+++ src/com/company/DamageTyp.java
@@ -0,0 +1,5 @@
+package com.company;
+
+public enum DamageTyp {
+    SLASHING, MISSILE, BLUNT, NONE, PIERCING
+}
---
+++ src/com/company/Weapons.java
@@ -0,0 +1,64 @@
+package com.company;
+
+public class Weapons {
+    private String name;
+    private CombatType combatType;
+    private DamageTyp damageType;
+    private int damage;
+    private int speed;
+    private int strength;
+    private int value;
+
+    public Weapons(String name, CombatType combatType, DamageTyp damageType, int damage, int speed, int strength, int value) {
+        this.name = name;
+        this.combatType = combatType;
+        this.damageType = damageType;
+        this.damage = damage;
+        this.speed = speed;
+        this.strength = strength;
+        this.value = value;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public CombatType getCombatType() {
+        return combatType;
+    }
+    public void setCombatType(CombatType combatType) {
+        this.combatType = combatType;
+    }
+    public DamageTyp getDamageType() {
+        return damageType;
+    }
+    public void setDamageType(DamageTyp damageType) {
+        this.damageType = damageType;
+    }
+    public int getDamage() {
+        return damage;
+    }
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+    public int getSpeed() {
+        return speed;
+    }
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+    public int getStrength() {
+        return strength;
+    }
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+    public int getValue() {
+        return value;
+    }
+    public void setValue(int value) {
+        this.value = value;
+    }
+}
