f0e111b3257e20056e1f0008d05b0e9008adc22f
ZahrhuberV <valentin.zahrhuber@gmx.at>
2022-10-02 21:18:26 +0200

    Beispiel 3 verbessert

--- src/com/beispiel3/Beispiel3.java
+++ src/com/beispiel3/Beispiel3.java
@@ -9,29 +9,33 @@ public class Beispiel3 {
     public static void main(String[] args) {
         final Predicate<Integer> isEven = (n) -> (n%2 == 0);
         final IntPredicate isPositive = (n) -> (n > 0);
+        final Predicate<Integer> isZero = (n) -> (n == 0);
+        final Predicate<Integer> isNull = n -> (n == null);
 
-        Scanner s = new Scanner(System.in);
-        int input = 0;
-        do {
-            System.out.println("Gib eine Zahl ein");
-            System.out.println("1: Beenden");
-            input = s.nextInt();
-            if(isEven.test(input)){
-                System.out.println("Ist Gerade");
-            } else System.out.println("ist nicht gerade");
-            if(isPositive.test(input)){
-                System.out.println("Ist Positiv");
-            } else System.out.println("ist nicht positiv");
+        final Predicate<String> isShortWord = n -> (n.length() < 4);
 
-            System.out.println("Beides gemeinsam getestet: ");
-            if(isEven.and(n -> n>0).test(input)){
-                System.out.println("Gerade und positiv");
-            } else System.out.println("ist nicht gerade und negativ");
+        System.out.println("Gerade?");
+        System.out.println(isEven.test(11));
+        System.out.println(isEven.test(10));
+        System.out.println("Positive?");
+        System.out.println(isPositive.test(-10));
+        System.out.println(isPositive.test(10));
+        System.out.println("0?");
+        System.out.println(isZero.test(0));
+        System.out.println(isZero.test(10));
+        System.out.println("Null?");
+        System.out.println(isNull.test(null));
+        System.out.println(isNull.test(0));
+        System.out.println("<4 buchstaben");
+        System.out.println(isShortWord.test("Valentin"));
+        System.out.println(isShortWord.test("Zah"));
+        System.out.println("mit and Methode");
+        System.out.println(isEven.and(n -> n > 0).test(10));
+        System.out.println(isEven.and(n -> n > 0).test(11));
+        System.out.println(isEven.and(n -> n > 0).test(-10));
+        System.out.println("mit methode negate");
+        System.out.println(isEven.negate().test(11));
+        System.out.println(isEven.negate().test(10));
 
-            System.out.println("Negate benutzt:");
-            if(!(isEven.negate().test(input))){
-                System.out.println("Ungerade und negativ");
-            } else System.out.println("gerade und positiv");
-        }while(input!=1);
     }
 }
